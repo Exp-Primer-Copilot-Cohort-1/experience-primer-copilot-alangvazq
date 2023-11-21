7280b0c86b4ca78a3e7e7269a7b660750d5304f7
alangvazq <109816234+alangvazq@users.noreply.github.com>
2023-11-21 19:27:53 +0000

    copilot member

--- member.js
+++ member.js
@@ -1,3 +1,6 @@
 function skillsMember() {
-    // Your code here
+  var member = document.getElementById('member');
+  var memberText = document.getElementById('member-text');
+  member.style.display = 'block';
+  memberText.style.display = 'none';
 }
